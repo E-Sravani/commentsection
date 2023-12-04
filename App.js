66771e41eac04cdeb968f88db30d5f43b0d894ef
E-Sravani <135720121+E-Sravani@users.noreply.github.com>
2023-12-04 22:38:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Home from './home';
+import Student from './student';
+import StudentList from './studentlist';
+import NestedComment from './commentsection';
+function App() {
+  return (
+  // <div>
+  //   <h1>hello react app</h1> 
+  // </div> 
+  // <Home/>
+  // <Student/>
+  // <StudentList/>
+<NestedComment/>
+  
+
+  );
+}
+
+export default App;
